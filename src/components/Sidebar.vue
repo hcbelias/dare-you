a697b7e1750cba19f0482e0e4bde70b532aad3ce
Henrique Elias <hcbe2004@gmail.com>
2020-05-26 18:57:55 -0300

    creating sidebar and toolbar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,156 @@
+
+<template>
+  <q-drawer
+    v-model="leftDrawerOpen"
+    show-if-above
+    :width="200"
+    :breakpoint="400"
+  >
+    <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+      <q-list padding>
+        <q-item
+          clickable
+          v-ripple
+        >
+          <q-item-section avatar>
+            <q-icon name="inbox" />
+          </q-item-section>
+
+          <q-item-section>
+            Inbox
+          </q-item-section>
+        </q-item>
+
+        <q-item
+          active
+          clickable
+          v-ripple
+        >
+          <q-item-section avatar>
+            <q-icon name="star" />
+          </q-item-section>
+
+          <q-item-section>
+            Star
+          </q-item-section>
+        </q-item>
+
+        <q-item
+          clickable
+          v-ripple
+        >
+          <q-item-section avatar>
+            <q-icon name="send" />
+          </q-item-section>
+
+          <q-item-section>
+            Send
+          </q-item-section>
+        </q-item>
+
+        <q-item
+          clickable
+          v-ripple
+        >
+          <q-item-section avatar>
+            <q-icon name="drafts" />
+          </q-item-section>
+
+          <q-item-section>
+            Drafts
+          </q-item-section>
+        </q-item>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-scroll-area>
+
+    <q-img
+      class="absolute-top"
+      src="https://cdn.quasar.dev/img/material.png"
+      style="height: 150px"
+    >
+      <div class="absolute-bottom bg-transparent">
+        <q-avatar
+          size="56px"
+          class="q-mb-sm"
+        >
+          <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+        </q-avatar>
+        <div class="text-weight-bold">Henrique Elias</div>
+        <div>@hcbelias</div>
+      </div>
+    </q-img>
+  </q-drawer>
+</template>
+
+<script>
+import EssentialLink from './EssentialLink'
+
+export default {
+  name: 'Sidebar',
+  props: {
+    leftDrawerOpen: Boolean
+  },
+  components: [
+    EssentialLink
+  ],
+  data () {
+    return {
+      essentialLinks: [
+        {
+          title: 'Docs',
+          caption: 'quasar.dev',
+          icon: 'school',
+          link: 'https://quasar.dev'
+        },
+        {
+          title: 'Github',
+          caption: 'github.com/quasarframework',
+          icon: 'code',
+          link: 'https://github.com/quasarframework'
+        },
+        {
+          title: 'Discord Chat Channel',
+          caption: 'chat.quasar.dev',
+          icon: 'chat',
+          link: 'https://chat.quasar.dev'
+        },
+        {
+          title: 'Forum',
+          caption: 'forum.quasar.dev',
+          icon: 'record_voice_over',
+          link: 'https://forum.quasar.dev'
+        },
+        {
+          title: 'Twitter',
+          caption: '@quasarframework',
+          icon: 'rss_feed',
+          link: 'https://twitter.quasar.dev'
+        },
+        {
+          title: 'Facebook',
+          caption: '@QuasarFramework',
+          icon: 'public',
+          link: 'https://facebook.quasar.dev'
+        },
+        {
+          title: 'Quasar Awesome',
+          caption: 'Community Quasar projects',
+          icon: 'favorite',
+          link: 'https://awesome.quasar.dev'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <q-header elevated>
+    <q-toolbar class="bg-primary text-white">
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="toggle"
+      />
+
+      <q-toolbar-title>Dare you</q-toolbar-title>
+      <q-btn
+        flat
+        round
+        dense
+        icon="message"
+      />
+      <q-btn
+        flat
+        round
+        dense
+        icon="account_circle"
+      />
+      <q-btn
+        flat
+        round
+        dense
+        icon="settings"
+      />
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+  props: {
+    toggle: Function
+  }
+}
+</script>
